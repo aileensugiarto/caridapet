7924818a44e55ab7ad2e886c121d39ca8b2c05d1
aileensugiarto <aileensugiarto07@gmail.com>
2026-06-22 12:01:29 +0700

    Initial commit

---
+++ products.js
@@ -0,0 +1,20 @@
+const products = [
+    {
+        id_product: 1,
+        name: "Payung Mobil",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTBlXvDH2faxVwUYfc7XdVcXxAlHNFGLK-upXL0DkTJBOeCcJU2rDxmuCD&s=10",
+        link: "https://s.shopee.co.id/8fQAOt6bmC"
+    },
+    {
+        id_product: 2,
+        name: "Phone Holder Magnet",
+        image: "https://down-id.img.susercontent.com/file/id-11134207-7ra0s-mdrgu04dqdhn16.webp",
+        link: "https://s.shopee.co.id/4qDRpv4g16"
+    },  
+    {
+        id_product: 3,
+        name: "Gantungan Kursi Mobil",
+        image: "https://down-id.img.susercontent.com/file/sg-11134275-81zw9-mie9z41m26mb9d@resize_w900_nl.webp",
+        link: "https://s.shopee.co.id/2g8xG4SfPs"
+    },
+];
---
+++ script.js
@@ -0,0 +1,32 @@
+const container = document.getElementById("productsContainer");
+const searchInput = document.getElementById("searchInput");
+
+function displayProducts(items) {
+    container.innerHTML = "";
+    const sorted = [...items].sort((a, b) => b.id_product - a.id_product);
+    sorted.forEach(product => {
+        container.innerHTML += `
+            <div class="product-card">
+                <a href="${product.link}" target="_blank">
+                    <img src="${product.image}" alt="${product.name}">
+                    <div class="product-info">
+                        <h3>${product.name}</h3>
+                    </div>
+                </a>
+                
+            </div>
+        `;
+    });
+}
+
+displayProducts(products);
+
+searchInput.addEventListener("input", () => {
+    const value = searchInput.value.toLowerCase();
+
+    const filtered = products.filter(product =>
+        product.name.toLowerCase().includes(value)
+    );
+
+    displayProducts(filtered);
+});
