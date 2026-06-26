036fef940bf87496532fa5943b86e5e13bde6a5f
aileensugiarto <aileensugiarto07@gmail.com>
2026-06-26 18:21:01 +0700

    update products

--- products.js
+++ products.js
@@ -53,4 +53,10 @@ const products = [
         image: "https://m.media-amazon.com/images/I/517g1OFHK4L._AC_UF894,1000_QL80_.jpg",
         link: "https://s.shopee.co.id/7KusjNouDx"
     },
+    {
+        id_product: 10,
+        name: "Crawling Shinchan",
+        image: "https://happyhomesonline.com/cdn/shop/files/HH6574_2.jpg?v=1768375947&width=416",
+        link: "https://s.shopee.co.id/70I3lQ0jih"
+    },
 ];
