219c233d30ace46318bf04aebabb0a6b0364f143
aileensugiarto <aileensugiarto07@gmail.com>
2026-06-27 11:19:29 +0700

    update products

--- products.js
+++ products.js
@@ -59,4 +59,10 @@ const products = [
         image: "https://happyhomesonline.com/cdn/shop/files/HH6574_2.jpg?v=1768375947&width=416",
         link: "https://s.shopee.co.id/70I3lQ0jih"
     },
+    {
+        id_product: 11,
+        name: "Penguin Egg Boiler",
+        image: "https://m.media-amazon.com/images/I/71itgZm7n1L._AC_UF894,1000_QL80_.jpg",
+        link: "https://s.shopee.co.id/50X0whDkNo"
+    },
 ];
