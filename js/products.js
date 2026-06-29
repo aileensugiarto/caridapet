697d5490f8c8d8312a70045695cb76268d6ad053
aileensugiarto <aileensugiarto07@gmail.com>
2026-06-29 11:23:51 +0700

    update products

--- js/products.js
+++ js/products.js
@@ -7,7 +7,6 @@ const products = [
         amazon: "https://amzn.to/4xRggY9",
         shopee: "https://s.shopee.co.id/7KusjNouDx"
     },
-
     {
         slug: "crawling-shinchan",
         name: "Crawling Shinchan",
@@ -16,7 +15,6 @@ const products = [
         amazon: "https://amzn.to/4aq0zgD",
         shopee: "https://s.shopee.co.id/70I3lQ0jih"
     },
-
     {
         slug: "penguin-egg-boiler",
         name: "Penguin Egg Boiler",
@@ -25,7 +23,6 @@ const products = [
         amazon: "https://amzn.to/3SAw8OI",
         shopee: "https://s.shopee.co.id/50X0whDkNo"
     },
-
     {
         slug: "fries-bag-clips",
         name: "Fries Bag Clips",
@@ -33,5 +30,13 @@ const products = [
         image: "https://assets.target.com.au/transform/cb71fa2f-0639-4c38-a973-6e6c7368d922/43476428-2?io=transform%3Afit%2Cwidth%3A1400%2Cheight%3A1600&quality=90&output=webp",
         amazon: "https://amzn.to/4v7kCYJ",
         shopee: "https://s.shopee.co.id/9Kg28mSa3O"
-    }
+    },
+    {
+        slug: "shiba-wall-protector",
+        name: "Shiba Wall Protector",
+        description: "Cute soft silicone wall protector that absorbs door impacts, reduces noise, and helps prevent wall damage.",
+        image: "https://m.media-amazon.com/images/I/71UssfHmtHL._AC_UF894,1000_QL80_.jpg",
+        amazon: "https://amzn.to/442rfAs",
+        shopee: "https://s.shopee.co.id/W4epZ1ECk"
+    },
 ];
